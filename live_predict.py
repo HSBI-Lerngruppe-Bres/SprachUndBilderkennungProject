8c3ce5a2a1eb01acc97dc1b612adaf97875eb66c
Nils Baxheinrich <nils@nbax.de>
2024-06-05 13:14:36 +0200

    training 300 epochs

--- live_predict.py
+++ live_predict.py
@@ -1,9 +1,7 @@
-import cv2
 from ultralytics import YOLO
-# from ultralytics.yolo.utils import render_result
-
+import cv2
 # Load your trained YOLO classification model
-model = YOLO('path_to_your_trained_model.pt')
+model = YOLO('runs/classify/train6/weights/best.pt')
 
 # Open a connection to the webcam (or other video source)
 cap = cv2.VideoCapture(0)  # 0 is the default camera
@@ -19,14 +17,29 @@ while True:
         print("Error: Could not read frame.")
         break
 
-    # Run the frame through the YOLO model for classification
-    results = model(frame)
+    # Convert the frame to the format expected by the model
+    input_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # input_frame = cv2.resize(frame_gray, (model.img_size, model.img_size))
 
-    # Render the results on the frame
-    model.render_result(results, frame)
+    # Run the frame through the YOLO model for classification
+    results = model(input_frame)
+
+    # Assuming the model returns a list of results with class names and confidence scores
+    for result in results:
+        # Convert probabilities to numpy array if needed
+        probs = result.probs.cpu().numpy()
+
+        # Get the index of the class with the highest probability
+        class_idx = probs.top1
+        print(model.names[class_idx])
+        # Get the class name and confidence
+        # label = result.names[class_idx]
+        # confidence = probs[class_idx]
+
+        # print(f"Class: {label}, Confidence: {confidence:.2f}")
 
     # Display the frame
-    cv2.imshow('Live Stream', frame)
+    cv2.imshow('Live Stream', input_frame)
 
     # Exit the loop when 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- train.py
+++ train.py
@@ -4,3 +4,4 @@ args = dict(model='yolov8n-cls.pt', data='V1', epochs=3)
 
 trainer = ClassificationTrainer(overrides=args)
 trainer.train()
+    
