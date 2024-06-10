940a9219cffdb330a019041729da0daaa6b8a401
Nils Baxheinrich <nils@nbax.de>
2024-06-10 20:31:00 +0200

    Everymodel run

--- train.py
+++ train.py
@@ -1,7 +1,12 @@
 from ultralytics.models.yolo.classify import ClassificationTrainer
 
-args = dict(model='yolov8n-cls.pt', data='V1', epochs=3)
+model_sizes = ['yolov8n-cls.pt', 'yolov8s-cls.pt',
+               'yolov8m-cls.pt', 'yolov8l-cls.pt', 'yolov8x-cls.pt']
+data = 'V1'
+epochs = 300
 
-trainer = ClassificationTrainer(overrides=args)
-trainer.train()
-    
+for model in model_sizes:
+    args = dict(model=model, data=data, epochs=epochs)
+    trainer = ClassificationTrainer(overrides=args)
+    trainer.train()
+    print(f"Training complete for model {model}")
